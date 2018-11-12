765858dbe3abdf753c0a95c0bb1c17added0000e
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2018-11-13 00:58:03 +0800

    rename kmp

--- src/string/kmp.cpp
+++ src/string/kmp.cpp
@@ -1,27 +1,33 @@
 const int SIZE = 10005;
-int fail[SIZE];
-int len;
 
-void construct(const char* p) {
-  len = strlen(p);
-  fail[0] = fail[1] = 0;
-  for (int i = 1; i < len; i++) {
-    int j = fail[i];
-    while (j && p[i] != p[j]) j = fail[j];
-    fail[i + 1] = p[i] == p[j] ? j + 1 : 0;
+struct kmp_matcher {
+  char p[SIZE];
+  int fail[SIZE];
+  int len;
+
+  void construct(const char* needle) {
+    len = strlen(p);
+    strcpy(p, needle);
+    fail[0] = fail[1] = 0;
+    for (int i = 1; i < len; i++) {
+      int j = fail[i];
+      while (j && p[i] != p[j]) j = fail[j];
+      fail[i + 1] = p[i] == p[j] ? j + 1 : 0;
+    }
   }
-}
 
-inline void found(int pos) {
-  // ! add codes for having found at pos
-}
-
-void match(const char* t, const char* p) {  // must be called after construct
-  int n = strlen(t);
-  int j = 0;
-  rep(i, n) {
-    while (j && p[j] != t[i]) j = fail[j];
-    if (p[j] == t[i]) j++;
-    if (j == len) found(i - len + 1);
+  inline void found(int pos) {
+    // ! add codes for having found at pos
   }
-}
+
+  void match(const char* haystack) {  // must be called after construct
+    const char* t = haystack;
+    int n = strlen(t);
+    int j = 0;
+    rep(i, n) {
+      while (j && p[j] != t[i]) j = fail[j];
+      if (p[j] == t[i]) j++;
+      if (j == len) found(i - len + 1);
+    }
+  }
+};

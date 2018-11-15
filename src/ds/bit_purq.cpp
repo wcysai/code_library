7b717a5d483553a42612f59691e90128e9b5054a
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2018-11-15 20:18:04 +0800

    misc changes

--- src/ds/bit_purq.cpp
+++ src/ds/bit_purq.cpp
@@ -1,26 +1,24 @@
-inline int lowbit(int x){return x&-x;}
-
-struct bit_purq{ // point update, range query
+struct bit_purq { // point update, range query
     int N;
     vector<LL> tr;
 
-    void init(int n){ // fill the array with 0
+    void init(int n) { // fill the array with 0
         tr.resize(N = n + 5);
     }
 
-    LL sum(int n){
+    LL sum(int n) {
         LL ans = 0;
-        while (n){
+        while (n) {
             ans += tr[n];
-            n -= lowbit(n);
+            n &= n - 1;
         }
         return ans;
     }
 
     void add(int n, LL x){
-        while (n < N){
+        while (n < N) {
             tr[n] += x;
-            n += lowbit(n);
+            n += n & -n;
         }
     }
 };
--- src/ds/bit_rupq.cpp
+++ src/ds/bit_rupq.cpp
@@ -1,26 +1,24 @@
-inline int lowbit(int x){return x&-x;}
-
 struct bit_rupq{ // range update, point query
     int N;
     vector<LL> tr;
 
-    void init(int n){ // fill the array with 0
+    void init(int n) { // fill the array with 0
         tr.resize(N = n + 5);
     }
 
-    LL query(int n){
+    LL query(int n) {
         LL ans = 0;
-        while (n < N){
+        while (n < N) {
             ans += tr[n];
-            n += lowbit(n);
+            n += n & -n;
         }
         return ans;
     }
 
-    void add(int n, LL x){
+    void add(int n, LL x) {
         while (n){
             tr[n] += x;
-            n -= lowbit(n);
+            n &= n - 1;
         }
     }
 };

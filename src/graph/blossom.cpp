476cb277726ae62aff02d6756437b4a8162d6de0
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2019-01-25 23:39:41 +0800

    add blossom algorithm

---
+++ src/graph/blossom.cpp
@@ -0,0 +1,87 @@
+const int MAXN = 1024;
+struct Blossom {
+    vector<int> adj[MAXN];
+    queue<int> q;
+    int n;  // set n to number of vertices before use
+    int label[MAXN], mate[MAXN], save[MAXN], used[MAXN];
+
+
+    void init(int nv) {
+        n = nv;
+        Rep (i, n) adj[i].clear();
+        memset(label, 0, sizeof label);
+        memset(mate, 0, sizeof mate);
+        memset(save, 0, sizeof save);
+        memset(used, 0, sizeof used);
+    }
+
+    void add_edge(int u, int v) {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    void rematch(int x, int y){
+        int m = mate[x]; mate[x] = y;
+        if (mate[m] == x) {
+            if (label[x] <= n) {
+                mate[m] = label[x];
+                rematch(label[x], m);
+            } else {
+                int a = 1 + (label[x] - n - 1) / n;
+                int b = 1 + (label[x] - n - 1) % n;
+                rematch(a, b); rematch(b, a);
+            }
+        }
+    }
+
+    void traverse(int x) {
+        Rep (i, n) save[i] = mate[i];
+        rematch(x, x);
+        Rep (i, n) {
+            if (mate[i] != save[i]) used[i]++;
+            mate[i] = save[i];
+        }
+    }
+
+    void relabel(int x, int y) {
+        Rep (i, n) used[i] = 0;
+        traverse(x); traverse(y);
+        Rep (i, n) {
+            if (used[i] == 1 and label[i] < 0) {
+                label[i] = n + x + (y - 1) * n;
+                q.push(i);
+            }
+        }
+    }
+
+    int solve() {
+        Rep (i, n) {
+            if (mate[i]) continue;
+            Rep (j, n) label[j] = -1;
+            label[i] = 0; q = queue<int>(); q.push(i);
+            while (q.size()) {
+                int x = q.front(); q.pop();
+                for (int y : adj[x]) {
+                    if (mate[y] == 0 and i != y) {
+                        mate[y] = x;
+                        rematch(x, y);
+                        q = queue<int>();
+                        break;
+                    }
+                    if (label[y] >= 0) {
+                        relabel(x, y);
+                        continue;
+                    }
+                    if (label[mate[y]] < 0) {
+                        label[mate[y]] = x;
+                        q.push(mate[y]);
+                    }
+                }
+            }
+        }
+        int cnt = 0;
+        Rep (i, n) if (mate[i] > i) cnt++;
+        return cnt;
+    }
+};
+

70a6c23b705c781bba975701b4b58d2117e6761a
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2019-02-05 12:34:15 +0800

    update berlekamp-massey

--- src/math/berlekamp-massey.cpp
+++ src/math/berlekamp-massey.cpp
@@ -1,77 +1,22 @@
-const LL MOD = 1000000007;
-
-LL inverse(LL b) {
-  LL e = MOD - 2, r = 1;
-  while (e) {
-    if (e & 1) r = r * b % MOD;
-    b = b * b % MOD;
-    e >>= 1;
-  }
-  return r;
-}
-
-struct Poly {
-  vector<int> a;
-
-  Poly() { a.clear(); }
-
-  Poly(vector<int> &a) : a(a) {}
-
-  int length() const { return a.size(); }
-
-  Poly move(int d) {
-    vector<int> na(d, 0);
-    na.insert(na.end(), a.begin(), a.end());
-    return Poly(na);
-  }
-
-  int calc(vector<int> &d, int pos) {
-    int ret = 0;
-    for (int i = 0; i < (int)a.size(); ++i) {
-      if ((ret += (long long)d[pos - i] * a[i] % MOD) >= MOD) {
-        ret -= MOD;
-      }
+vector<int> berlekamp(const vector<int>& a) {
+    vector<int> p = {1}, r = {1};
+    int dif = 1;
+    rep (i, a.size()) {
+        int u = 0;
+        rep (j, p.size())
+            u = (u + 1ll * p[j] * a[i-j]) % mod;
+        if (u == 0) {
+            r.insert(r.begin(), 0);
+        } else {
+            auto op = p;
+            p.resize(max(p.size(), r.size() + 1));
+            int idif = inv(dif);
+            rep (j, r.size()) 
+                p[j+1] = 
+                    (p[j+1] - 1ll * r[j] * idif % mod * u % mod + mod) % mod;
+            dif = u;
+            r = op;
+        }
     }
-    return ret;
-  }
-
-  Poly operator - (const Poly &b) {
-    vector<int> na(max(this->length(), b.length()));
-    for (int i = 0; i < (int)na.size(); ++i) {
-      int aa = i < this->length() ? this->a[i] : 0,
-          bb = i < b.length() ? b.a[i] : 0;
-      na[i] = (aa + MOD - bb) % MOD;
-    }
-    return Poly(na);
-  }
-};
-
-Poly operator * (const int &c, const Poly &p) {
-  vector<int> na(p.length());
-  for (int i = 0; i < (int)na.size(); ++i) {
-    na[i] = (long long)c * p.a[i] % MOD;
-  }
-  return na;
-}
-
-vector<int> solve(vector<int> a) {
-  int n = a.size();
-  Poly s, b;
-  s.a.push_back(1), b.a.push_back(1);
-  for (int i = 1, j = 0, ld = a[0]; i < n; ++i) {
-    int d = s.calc(a, i);
-    if (d) {
-      if ((s.length() - 1) * 2 <= i) {
-        Poly ob = b;
-        b = s;
-        s = s - (long long)d * inverse(ld) % MOD * ob.move(i - j);
-        j = i;
-        ld = d;
-      } else {
-        s = s - (long long)d * inverse(ld) % MOD * b.move(i - j);
-      }
-    }
-  }
-  // Caution: s.a might be shorter than expected
-  return s.a;
+    return p;
 }
